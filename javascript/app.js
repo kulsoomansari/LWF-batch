9336d056dee1333406e1a07df26d830865ef22af
kulsoomansari <kulsoomansari412@gmail.com>
2022-02-28 20:40:21 +0500

    first commit

---
+++ javascript/app.js
@@ -0,0 +1,62 @@
+// variables =?
+// containers store value
+// creating a variable in js is called declaring a variable 
+// var a = 30
+// document.write(a)
+// document.write("<br>")
+// var b = 15
+// document.write(b)
+// var b = 45
+// var c = a + b
+// document.write(c);
+// var num1 = 10;
+// var num2 = "15"
+// var num = num1 + num2
+// document.write(num)
+// document.write("<br>")
+// document.write("<b>")
+// var word = "this is our \"first\" practice class";
+// document.write(word)
+ 
+var x = 9
+var y = x - 18
+document.write(y)
+document.write("<br>")
+var mul = y*5
+document.write(mul)
+document.write("<br>")
+var division = mul/ -5
+document.write(division)
+document.write("<br>")
+var mod = 71 % 6
+document.write(mod)
+document.write("<br>")
+
+var a = 1;
+var b = "1";
+// if(a===b){
+//     document.write("both values are equal")
+// } else{
+// document.write("both values are not equal")
+// }
+if(a!=b){
+    document.write("condition fulfilled")
+} else{
+    document.write("condition not fulfilled")
+}
+document.write("<br>")
+// var num = 5;
+// num = num + 6;
+// document.write(num)
+
+var num =5;
+num -= 7;
+document.write(num)
+
+var c = prompt("where do you live?")
+if(c =="Karachi"){
+    document.write("You are allowed")
+} else{
+    document.write("you are not allowed")
+}
+
---
+++ javascript/app1.js
@@ -0,0 +1,60 @@
+// var age =  prompt("what is your age")
+// if(age === 18){
+//     document.write("you are allowed")
+// } else if(age >=18){
+//     document.write("you are allowed 1 years ago")
+// }else {
+//     document.write("you are not allowed")
+// }
+
+// var firstName = "kulsoom"
+// var lastName = "ansari"
+// document.write(firstName + " " + lastName)
+// document.write("<br>")
+
+// var weight = prompt("what is your correct weight?")
+// var age = prompt("what is your age?")
+// var city = prompt("what is your residence?")
+
+// if ( age >=18 || weight < 98 && city === "Newyork"){
+//     document.write("you are allowed to visit")
+// } 
+// else {
+//     document.write("you are not allowed")  
+// }
+
+
+// alert("welcome to JS land \r\n happy coding")
+
+
+var a = 6
+var b= 5
+var x =7
+var y= 7
+var c= 6
+var d = 6
+var g =4
+var h = 2
+var e = 8
+var f = 5
+
+// if((x===y || a===b) && c===d){
+//     document.write(g=h)
+// }
+// else{
+//     document.write(e=f)
+// }
+
+if(c===d){
+    if(x===y){
+        document.write(g=h)
+    }
+    else if(a===b){
+document.write(g=h)
+    }
+    else{
+        document.write(e=f) 
+    }
+} else{
+    document.write(e=f) 
+}
